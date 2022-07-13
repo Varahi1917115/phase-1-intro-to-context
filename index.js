28dc5de5d9b2e92d7a50da6785d77af04cab943b
Samarth <Varahi1917115@gmail.com>
2022-07-12 21:19:00 -0400

    commit tree a74f27ec1ce925eb0948715d2fdeb3add88d1e76

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const createEmployeeRecord = (recArray) => {
+    return {
+        firstName: recArray[0],
+        familyName: recArray[1], 
+        title: recArray[2],
+        payPerHour: recArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+       
+    }
+}
+
+const createEmployeeRecords = (recordsArray) => {
+    return recordsArray.map(rec => createEmployeeRecord(rec))
+
+}
+
+const allWagesFor = function () {
+    const eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date 
+    })
+
+    const payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0)
+
+    return payable
+}
